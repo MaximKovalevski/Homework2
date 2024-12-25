0f4fdb9e4cfb233a7bc46a43974556190bb53790
Maksim Kovalevski <maksimkovalevski@gmail.com>
2024-12-25 17:05:21 +0200

    Homework2

--- Homework2.js
+++ Homework2.js
@@ -1,4 +1,4 @@
 //Task 6: variables with different types but same data
 let valueAsNumber = 7 //number
-let valueAsString = '7' //string
+let valueAsString = "7" //string
 console.log(valueAsNumber === valueAsString)
